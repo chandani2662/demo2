713a3417322abde439694eea68e2b5d86883c252
chandani2662 <achandani2662@gmail.com>
2019-08-13 13:12:07 +0530

    this is demo on git hub in group

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My demo on terminal and git hub</title>
+</head>
+<body>
+<h1>My demo on terminal and git hub</h1>
+<h3>this working in group</h3>
+</body>
+</html>
